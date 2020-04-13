3298a206159ce555cef2bf806127523ec7564185
sekou <sekou@sekous-MBP>
2020-04-13 14:12:30 +0300

    adding the challenge email to the setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='ushiriki_policy_engine_library',
       version='1.0',
       description='Python Langauge User facing library for accessing the Ushiriki Policy Engine webservice API',
       author='Ushiriki Policy Engine Library Developers',
-      author_email='n/a@.com',
+      author_email='malaria.ml.challenge@ke.ibm.com',
       url='https://github.com/IBM/ushiriki-policy-engine-library',
       packages=find_packages(),
 )
