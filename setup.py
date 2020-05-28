151bf8045801de706e63f52c4078889c72b333ee
slremy <sekouremy@gmail.com>
2020-05-28 09:39:39 +0300

    Update setup.py
    requests is an uninstalled dependency for gym it seems :(

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='ushiriki_policy_engine_library',
       author='Ushiriki Policy Engine Library Developers',
       author_email='malaria.ml.challenge@ke.ibm.com',
       url='https://github.com/IBM/ushiriki-policy-engine-library',
-      install_requires = ["gym"],
+      install_requires = ["gym", "requests"],
       packages=find_packages(),
 )
