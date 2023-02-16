e84d78a8b19583e9bed7bd1dcc0bda6c72906603
Sekou L Remy <sekou.lionel.remy@ke.ibm.com>
2023-02-16 09:19:51 -0600

    iclr23workshop extension

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(name='ushiriki_policy_engine_library',
-      version='1.0',
+      version='1.1',
       description='Python Langauge User facing library for accessing the Ushiriki Policy Engine webservice API',
       author='Ushiriki Policy Engine Library Developers',
       author_email='malaria.ml.challenge@ke.ibm.com',
       url='https://github.com/IBM/ushiriki-policy-engine-library',
-      install_requires = ["gym", "requests"],
+      install_requires = ["gym", 
+          "requests", 
+          "samplemetamodel @ git+https://github.com/Model-Driven-Discovery-Workshop/samplemetamodel",
+          "covasimmetamodels @ git+https://github.com/Model-Driven-Discovery-Workshop/covasimmetamodels"],
       packages=find_packages(),
 )
